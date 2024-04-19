4ff84e991110c796dc3065955984234e00da7a43
Havkost <38227712+Havkost@users.noreply.github.com>
2024-04-19 10:44:16 +0200

    feat(web): Add hardcoded midi_to_wav.py

--- web_server/midi_to_wav.py
+++ web_server/midi_to_wav.py
@@ -11,24 +11,13 @@ def midi_to_wav(midi_file, output_dir):
 
     # Convert MIDI to WAV
     wav_file = os.path.join(output_dir, os.path.splitext(os.path.basename(midi_file))[0] + '.wav')
-    print("Converting MIDI to WAV:", midi_file, "=>", wav_file)
     fluidsynth.midi_to_audio(midi_file, wav_file)
 
     return wav_file
 
-# Example usage
-'''
-midi_file = "bach_846.mid"
-output_dir = "output_directory"
-wav_file = midi_to_wav(midi_file, output_dir)
-print("WAV file generated:", wav_file)
-'''
+# Set output_dir as audio_bucket in the current directory
+output_dir = os.path.join(os.getcwd(), 'audio_bucket')
 
-# Convert all MIDI files in a directory to WAV
-midi_dir = "C:\\University\\dataset_sw6\\piano_midi"
-output_dir = "C:\\University\\dataset_sw6\\constructed_wav"
-for midi_file in os.listdir(midi_dir):
-    if midi_file.endswith(".mid"):
-        midi_file = os.path.join(midi_dir, midi_file)
-        wav_file = midi_to_wav(midi_file, output_dir)
-        print("WAV file generated:", wav_file)
+# Set toms_diner1.mid as the input MIDI file
+midi_file = os.path.join(output_dir, 'toms_diner1.mid')
+wav_file = midi_to_wav(midi_file, output_dir)
